09c02752710edf50c6dd69c4d2f556251865e346
blitz-jgm <jongdudu1@gmail.com>
2026-03-02 13:44:31 +0900

    environment setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+
+def main():
+    print("Hello from langchain-course!")
+    print(f"Your API key is: {os.environ.get('ANTHROPIC_API_KEY')}")
+
+
+if __name__ == "__main__":
+    main()
