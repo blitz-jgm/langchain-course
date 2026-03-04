8c377172cfcb6192a95ab2a985128da77c9d967a
blitz-jgm <jongdudu1@gmail.com>
2026-03-04 21:59:19 +0900

    Add response format to agent and define data models for source and agent response

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,11 +9,23 @@ from langchain_core.messages import HumanMessage
 from langchain_anthropic import ChatAnthropic
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Scheme for a source used by the agent"""
+    
+    url:str = Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """Scheme for the response of the agent with answer and sources"""
+    
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="The list ofsources used to generate the answer")
+
+
 
 
 llm = ChatAnthropic(model="claude-haiku-4-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
